e87f807a3eef01af6540436521a8f14a3b8da4a8
andersc <anderscui@gmail.com>
2015-01-29 14:19:20 +0800

    Add inline edit for action subject.

--- source/static/scripts/gtd.js
+++ source/static/scripts/gtd.js
@@ -54,3 +54,32 @@ gtd.status.find = function (id) {
 
     return null;
 };
+
+$.fn.inlineEdit = function(replaceWith, connectWith, callback) {
+
+    $(this).hover(function() {
+        $(this).addClass('hover');
+    }, function() {
+        $(this).removeClass('hover');
+    });
+
+    $(this).click(function() {
+
+        var elem = $(this);
+
+        elem.hide();
+        elem.after(replaceWith);
+        replaceWith.focus();
+
+        replaceWith.blur(function() {
+
+            if ($(this).val() != "") {
+                connectWith.val($(this).val()).change();
+                elem.text($(this).val());
+            }
+
+            $(this).remove();
+            elem.show();
+        });
+    });
+};
